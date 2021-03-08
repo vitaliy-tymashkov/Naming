9cdc71cf47fe3e780eb19b5c1e8164c4487c2c47
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-08 15:03:41 +0100

    Task 3 - moderate refactoring, variables renaming

--- src/test/java/com/epam/engx/cleancode/naming/task3/HarshadTestHelper.java
+++ src/test/java/com/epam/engx/cleancode/naming/task3/HarshadTestHelper.java
@@ -3,6 +3,6 @@ package com.epam.engx.cleancode.naming.task3;
 public class HarshadTestHelper {
 
     public static String getHarshadNumbers() {
-        return new H().main();
+        return new HarshadNumbers().main();
     }
 }
